c174df2e88c8eb2915331ed9c129c7de83e47ea0
fr0sty99 <j.neber@gmx.ch>
2018-06-02 05:30:28 +0200

    added Unity Project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Space Pace
+
+In my years of youth i used to play this very very (very) cool minigame for hours. I loved it so much. Today i cant find it so i'm trying to reproduce my own version of it. 
+
+Let's go! :)
+
+June 2. 2018
+-----
+planned for V 0.1:
+
+* Primitive Ship Model
+* User Input
+* Smooth Movement
+
+
+TODO: brainstorm
---
+++ Space Pace/Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float clockwise = 1000.0f;
+    public float counterClockwise = -5.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, Time.deltaTime * clockwise, 0);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, Time.deltaTime * counterClockwise, 0);
+        }
+    }
+}
