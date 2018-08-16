e07b97eacc287fd767778a510a6afd4b77a6140d
fr0sty99 <j.neber@gmx.ch>
2018-08-16 03:12:12 +0200

    WIP

---
+++ Bush_Sniper_V1.0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]     // This script requires the Gameobject to have a PlayerMotor component
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]    // When you mark a variable with "SerializeField", it will show up in the inspector, even tho it's a private variable
+    private float speed = 5f;
+
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        // no checking for errors needed because we use RequireComponent
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement velocity as a 2D Vector
+        // get user input
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _yMov = Input.GetAxisRaw("Vertical");
+
+        // calculate seperated x- and y- axis velocity
+        Vector2 _moveVertical = transform.up * _yMov;
+        Vector2 _moveHorizontal = transform.right * _xMov;
+
+        // calculate final movement vector and make it a normalized magnitude, which means the length of the Vector will always be 1, before multiplying it with the players speed
+        Vector2 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        // apply movement
+        motor.Move(_velocity);
+
+        // Calculatre rotation as a 2D Vector
+        Vector2 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("mousePos: " + _mousePos);
+
+        Vector2 _playerPos = transform.position;
+        Debug.Log("playerPos: " + _playerPos);
+
+        // for calculating the angle between the two points we need this formula from https://www.mathworks.com/matlabcentral/answers/180131-how-can-i-find-the-angle-between-two-vectors-including-directional-information
+        // angle = atan2d(x*_mousePos.y-_playerPos.y*_mousePos.x,_playerPos.x*_mousePos.x+_playerPos.y*_mousePos.y);
+        float angle = Mathf.Atan2(_playerPos.x * _mousePos.y - _playerPos.y * _mousePos.x, _playerPos.x * _mousePos.x + _playerPos.y * _mousePos.y);
+
+        Debug.Log("RotationAngle: " + angle);
+        motor.Rotate(angle);
+
+
+        // TODO: move gun and change it's angle relative to playerPosition and mousePosition     
+        // get position of mouse inside of World, we have to convert it with screenToWorldPoint f.e.
+        // get position of player
+        // rotate gun according to mouse position (angle and position)
+
+    }
+}
---
+++ Bush_Sniper_V1.0/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]     // This script requires the GameObject to have a Rigidbody2D component
+public class PlayerMotor : MonoBehaviour
+{
+
+    private Vector2 velocity = Vector2.zero;
+    private float rotationAngle = 0.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // we don't need to check for errors here, because we use RequireComponent
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Gets a movement vector
+    public void Move(Vector2 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    // Gets a rotation Vector
+    public void Rotate(float _rotationAngle)
+    {
+        rotationAngle = _rotationAngle;
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    // Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity != (Vector2.zero))
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.rotation += rotationAngle;
+        Debug.Log("PerformRotation -- MoveRotation: " + rotationAngle);
+    }
+}
